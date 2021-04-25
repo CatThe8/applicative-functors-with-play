800c701f1a66006d510d76799b0f6930f1365947
Damian Rafael Lattenero <damian.lattenero@rappi.com>
2021-04-25 06:07:41 -0300

    DONE

--- test/applicatives/ApplicativeFunctorTest.scala
+++ test/applicatives/ApplicativeFunctorTest.scala
@@ -14,7 +14,7 @@ class ApplicativeFunctorTest extends PlaySpec with GuiceOneAppPerTest with Injec
 
   "HomeController GET" should {
 
-    "render the index page from a new instance of controller" in {
+    "applicatives with apply over" in {
       val function: Int => Int => String => String = { (x: Int) => y: Int => z: String => s"$x - $y - $z" }
       val f1 = Future(3)
       val f2 = Future(4)
@@ -24,6 +24,42 @@ class ApplicativeFunctorTest extends PlaySpec with GuiceOneAppPerTest with Injec
         .applyOver(f1)
         .applyOver(f2)
         .applyOver(f3)
+
+      val result = Await.result(
+        applicativeResult, 10 seconds
+      )
+
+      println(result)
+    }
+
+    "applicatives with zip with" in {
+      val function: Int => Int => String => String = { (x: Int) => y: Int => z: String => s"$x - $y - $z" }
+      val f1 = Future(3)
+      val f2 = Future(4)
+      val f3 = Future("5")
+
+      val applicativeResult = Future(function)
+        .zipOver(f1)
+        .zipOver(f2)
+        .zipOver(f3)
+
+      val result = Await.result(
+        applicativeResult, 10 seconds
+      )
+
+      println(result)
+    }
+
+    "applicatives with zip with and apply over" in {
+      val function: Int => Int => String => String = { (x: Int) => y: Int => z: String => s"$x - $y - $z" }
+      val f1 = Future(3)
+      val f2 = Future(4)
+      val f3 = Future("5")
+
+      val applicativeResult = Future(function)
+        .zipOver(f1)
+        .applyOver(f2)
+        .zipOver(f3)
 
       val result = Await.result(
         applicativeResult, 10 seconds
